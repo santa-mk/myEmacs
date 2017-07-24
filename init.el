419d91068ec5c90bfd9dc25fbe4fec16db2e51e8
santa-mk <maguielaw.elir.107@gmail.com>
2017-07-24 11:06:05 +0900

    add settings

--- init.el
+++ init.el
@@ -14,3 +14,19 @@
 (cask-initialize)
 
 (keyboard-translate ?\C-h ?\C-?)
+
+(load "~/.emacs.d/rust.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (helm yasnippet web-mode use-package smex smartparens projectile prodigy popwin pallet nyan-mode multiple-cursors magit idle-highlight-mode htmlize flycheck-cask expand-region exec-path-from-shell drag-stuff))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
